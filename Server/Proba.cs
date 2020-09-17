9863ec05a3b9aca8e1abe06441252df90aeaf157
Davud <Davud@DESKTOP-6UVP2EU>
2020-09-17 19:56:12 +0200

    Add project files.

---
+++ Server/Proba.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorNestoNesto.Server
+{
+    public class Proba : Hub
+    {
+        public void Foo(string poruka, int br)
+        {
+            for (int i = 0; i < br; i++)
+            {
+                Console.WriteLine($"Klijent kaze: {poruka}");
+            }
+        }
+    }
+}
